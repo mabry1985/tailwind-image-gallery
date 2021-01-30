47f831ada00152010438d618a329e70bcb45c5c9
Josh Mabry <mabry1985@gmail.com>
2021-01-30 13:31:38 -0800

    extract card to own component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import ImageCard from './components/ImageCard';
 
 function App() {
   const [images, setImages] = useState([]);
@@ -18,43 +19,11 @@ function App() {
   }, []);
 
   return (
-    <div className="flex justify-between">
-      <div className="max-w-sm rounded overflow-hidden shadow-lg">
-        <img
-          className="w-full"
-          src="https://source.unsplash.com/random"
-          alt="random pic"
-        />
-        <div className="px-6 py4">
-          <div className="font-bold text-purple-500 text-xl my-2">
-            Photo by Josh Mabry
-          </div>
-          <ul>
-            <li>
-              <strong>Views:</strong>
-              4000
-            </li>
-            <li>
-              <strong>Downloads:</strong>
-              4000
-            </li>
-            <li>
-              <strong>Likes:</strong>
-              4000
-            </li>
-          </ul>
-        </div>
-        <div className="px-6 py-4">
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semi-bold text-gray-700 mr-2">
-            #tag1
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semi-bold text-gray-700 mr-2">
-            #tag2
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semi-bold text-gray-700 mr-2">
-            #tag3
-          </span>
-        </div>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+        {images.map(image => (
+          <ImageCard key={image.id} image={image} />
+        ))}
       </div>
     </div>
   );
